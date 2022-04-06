200fbbf472fe65d4765fb459f485576bafbcdb54
varchakov16 <varchakov@visualfabriq.com>
2022-04-07 02:58:31 +0300

    missed_geo

--- tools/json_generator.py
+++ tools/json_generator.py
@@ -5,7 +5,7 @@ import csv
 def read_file(filename) -> list:
     rows = []
 
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         csvreader = csv.reader(file)
         header = next(csvreader)
         for row in csvreader:
@@ -15,31 +15,32 @@ def read_file(filename) -> list:
 
     return rows
 
+
 # GENERATES UNIQUE COORD VIEW
 def generate_coord_view_json(rows) -> dict:
     used_addresses = dict()
     stations_dict = dict()
 
     identifier = 1
-    
+
     for row in rows:
         key = (row[1] + row[2]).lower()
 
         if key not in used_addresses.keys():
             station = {
-                    "routes": [row[0]],
-                    "name": row[1],
-                    "street": row[2],
-                    "coords": (row[3], row[4])
-                }
+                "routes": [row[0]],
+                "name": row[1],
+                "street": row[2],
+                "coords": (row[3], row[4]),
+            }
 
             used_addresses[key] = identifier
             stations_dict[identifier] = station
             identifier += 1
-        
+
         else:
             stations_dict[used_addresses[key]]["routes"].append(row[0])
-    
+
     json_object = json.dumps(stations_dict, ensure_ascii=False)
 
     with open("stations.json", "w") as outfile:
@@ -47,22 +48,23 @@ def generate_coord_view_json(rows) -> dict:
 
     return stations_dict
 
+
 # FIND STATIONS WITHOUT COORDS
 def stations_without_coords(stations_dict) -> list:
     no_coord_stations = list()
 
     for station in stations_dict.values():
-        if station['coords'][0] == "":
-            no_coord_stations.append(station['street'])
+        if station["coords"][0] == "":
+            no_coord_stations.append(station["street"])
 
     with open("no_coord_stations.txt", "w") as outfile:
         for street in no_coord_stations:
-            outfile.write(street+"\n")
+            outfile.write(street + "\n")
 
     return no_coord_stations
 
 
-if __name__ == '__main__':
-    data = read_file('./data/stations.csv')
+if __name__ == "__main__":
+    data = read_file("./data/stations.csv")
     stations_dict = generate_coord_view_json(data)
     todo = stations_without_coords(stations_dict)
