969be98df31269404e6007df0f94bb98da1d44a3
notnormasatall <taras.rodzin@ukr.net>
2022-04-12 20:07:54 +0300

    full routes regeneration

--- tools/find_coords.py
+++ tools/find_coords.py
@@ -3,17 +3,22 @@ import pandas as pd
 
 URL = f"https://nominatim.openstreetmap.org/search"
 
+
 def read_file():
     with open("./no_coord_stations.txt", "r", encoding="utf-8") as file:
         no_geo_stations = file.readlines()
 
         for i, line in enumerate(no_geo_stations):
-                if '(' in line: no_geo_stations[i] = line[line.index('.')+1:line.index('(')]
-                elif '.' in line: no_geo_stations[i] = line[line.index('.')+1:-1]
-                else: no_geo_stations[i] = line[:-1]
+            if '(' in line:
+                no_geo_stations[i] = line[line.index('.')+1:line.index('(')]
+            elif '.' in line:
+                no_geo_stations[i] = line[line.index('.')+1:-1]
+            else:
+                no_geo_stations[i] = line[:-1]
 
         for i, line in enumerate(no_geo_stations):
-            if '.' in line: no_geo_stations[i] = line[line.index('.')+1:]
+            if '.' in line:
+                no_geo_stations[i] = line[line.index('.')+1:]
 
     return no_geo_stations
 
@@ -26,7 +31,8 @@ def get_geo_information(stations):
         js_result = request_result.json()
 
         try:
-            geo_info.append([station, js_result[0]['lat'], js_result[0]['lon']])
+            geo_info.append(
+                [station, js_result[0]['lat'], js_result[0]['lon']])
         except IndexError:
             geo_info.append([station, "", ""])
 
