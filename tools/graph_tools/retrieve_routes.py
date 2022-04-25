6f731a96158b8c37032baa232a7e1f3d68579f1d
notnormasatall <taras.rodzin@ukr.net>
2022-04-26 01:24:21 +0300

    retrieve update

--- tools/graph_tools/retrieve_routes.py
+++ tools/graph_tools/retrieve_routes.py
@@ -2,35 +2,24 @@ from asyncio import current_task
 import json
 
 def retrieve_routes():
-    with open('./jsons/routes.json', 'r', encoding='utf-8') as f:
+    with open('./jsons/shapes_routes.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
-
+    
     # make list of tuples: [(stop1, stop2), (stop1, stop3)]
     # each stop: (lat, lon)
 
-    retrieved_even_routes = []
-    retrieved_odd_routes = []
-    even_route_names = []
-    odd_route_names = []
+    retrieved_routes = []
+    route_names = []
 
     for key in data.keys():
-        odd = True if 'odd' in key else False
         route_stops = []
-        current_route = data[key]
-        current_stations = current_route['stations']
-
-        for station in current_stations:
-            st_coords = (float(station['coords'][0]), float(station['coords'][1]))
-            route_stops.append(st_coords)
-
-        if odd:
-            retrieved_odd_routes.append(route_stops)
-            odd_route_names.append(key)
-        else:
-            retrieved_even_routes.append(route_stops)
-            even_route_names.append(key)
+        current_stations = data[key]
     
-    return retrieved_odd_routes, odd_route_names, retrieved_even_routes, even_route_names
+        for station in current_stations:
+            st_coords = (float(station[0]), float(station[1]))
+            route_stops.append(st_coords)
+        
+        retrieved_routes.append(route_stops)
+        route_names.append(key)
 
-# odd_routes, odd_names, even_routes, even_names = retrieve_routes()
-
+    return retrieved_routes, route_names
