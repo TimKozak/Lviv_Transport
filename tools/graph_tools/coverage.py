af871976534b560c765a388633b142a33bb9c115
Alina Voronina <alina.voronina@ucu.edu.ua>
2022-04-26 02:17:33 +0300

    coverage update

--- tools/graph_tools/coverage.py
+++ tools/graph_tools/coverage.py
@@ -4,30 +4,18 @@ import folium
 from retrieve_routes import retrieve_routes 
 
 def display_coverage(mins):
-    odd_routes, odd_names, even_routes, even_names = retrieve_routes()
-    odd_coords = [elem for route in odd_routes for elem in route]
-    even_coords = [elem for route in even_routes for elem in route]
+    routes, names = retrieve_routes()
+    coords = [elem for route in routes for elem in route]
 
     map = folium.Map(location=(49.8397, 24.0297), zoom_start=12) #Lviv
 
-    for i in range(len(odd_coords)):
-        lat, lon = odd_coords[i]
+    for i in range(len(coords)):
+        lat, lon = coords[i]
 
         folium.Circle([lat, lon],
                         radius=75*mins, #100 metres per minute
                         color='#3186cd',
-                        fill_color='#3186cc',
-                        opacity=0.5,
-                        fill_opacity=0.3
-                    ).add_to(map)
- 
-    for i in range(len(even_coords)):
-        lat, lon = even_coords[i]
-
-        folium.Circle([lat, lon],
-                        radius=75*mins, #100 metres per minute
-                        color = '#cd3186',
-                        fill_color='#cc3186',
+                        fill_color='#3186cd',
                         opacity=0.5,
                         fill_opacity=0.3
                     ).add_to(map)
